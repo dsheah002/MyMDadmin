abdfdbd887b9d09c8df4e2c34722164ac55aacda
dsheah002 <85600959+dsheah002@users.noreply.github.com>
2022-05-12 08:48:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def make_shell_context():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+        app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
